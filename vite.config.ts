8eb03e008e045bdcd389625484d1e694b07194e5
Shahan M Iqbal <mehtabi@clemson.edu>
2025-12-17 16:57:32 -0500

    updated library link

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,12 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
-import path from 'path';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-    const isDev = mode === 'development';
-
-    const alias = isDev
-        ? {
-            'rssa-study-template': path.resolve(
-                process.cwd(),
-                '../../libraries/rssa-study-template/src/index.ts'
-            ),
-        }
-        : undefined;
-
+export default defineConfig(() => {
     return {
         plugins: [react(), tailwindcss()],
         base: '/preference-community/',
-        resolve: {
-            alias,
-        },
         build: {
             outDir: 'build',
         },
